4685dc9cf7144029275abf5c2c7442701d4f0149
Maciej Wołosewicz <maciej.wolosewicz@gmail.com>
2021-02-24 18:50:59 +0100

    Change to C++ project

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,60 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,signed portCHAR *pcTaskName);
 
 void vApplicationStackOverflowHook(xTaskHandle *pxTask,signed portCHAR *pcTaskName) {
-	(void)pxTask;
-	(void)pcTaskName;
-	for(;;);
+    (void)pxTask;
+    (void)pcTaskName;
+    for(;;);
 }
 
-static void task1(void *args) {
-	int i;
+void setup() {
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+}
 
-	(void)args;
+#ifdef __cplusplus
+}
+#endif
 
-	for (;;) {
-		gpio_toggle(GPIOC,GPIO13);
-		for (i = 0; i < 1000000; i++)
-			__asm__("nop");
-	}
+
+void App::run() {
+    for (;;) {
+        gpio_toggle(GPIOC,GPIO13);
+        for (int i = 0; i < 9000000; i++) {
+            __asm__("nop");
+        }
+    }
+}
+
+
+static void appTask(void *args) {
+    (void)args;
+
+    App app;
+    app.run();
 }
 
 int main(void) {
 
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Use this for "blue pill"
-	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+    setup();
 
-	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
-	vTaskStartScheduler();
-	for (;;);
+    xTaskCreate(appTask,"app",100,NULL,configMAX_PRIORITIES-1,NULL);
+    vTaskStartScheduler();
+    for (;;);
 
-	return 0;
+    return 0;
 }
 
 // End
---
+++ src/main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  Created on: Feb 24, 2021
+ *      Author: mw
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+/**
+ * This is the main application class.
+ */
+class App {
+public:
+    void run();
+};
+
+
+
+#endif /* SRC_MAIN_H_ */
